66cb69c154d13673a5fcfee370715678f9b3e7f5
Esteban BIRET-TOSCANO <100210730+EstebanBiret@users.noreply.github.com>
2025-09-04 09:08:13 +0200

    code exo6

--- TP1/app.py
+++ TP1/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+#connect to MongoDB
+client = MongoClient("mongodb://mongo:27017/")
+db = client["testdb"]
+collection = db["messages"]
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+
+    #add a message to the database and count total messages (increment at each launch)
+    collection.insert_one({"message": "Hello World!"})
+    count = collection.count_documents({})
+    return f"Hello World! (nombre de messages en base: {count})"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
